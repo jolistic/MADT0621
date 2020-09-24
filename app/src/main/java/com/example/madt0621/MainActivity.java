5dd99b8382ca51a1f3f209b2942092f0fb0f75f4
Josiah Tan <55021839+jolistic@users.noreply.github.com>
2020-09-24 22:46:35 +0300

    Added function to change text color

--- app/src/main/java/com/example/madt0621/MainActivity.java
+++ app/src/main/java/com/example/madt0621/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.madt0621;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.tvMain = findViewById(R.id.tvMainLabel)
+        this.tvMain = findViewById(R.id.tvMainLabel);
     }
 
     public void onBtnChangeClick(View view) {
         this.tvMain.setText("We did something! Yay!");
+    }
+
+    public void onBtnChangeColor(View view) { this.tvMain.setTextColor(Color.RED);
     }
 }
