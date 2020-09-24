5084337205abbce763a359aeea22c548a1053b96
Josiah Tan <55021839+jolistic@users.noreply.github.com>
2020-09-24 22:34:55 +0300

    Added button and change textview function

--- app/src/main/java/com/example/madt0621/MainActivity.java
+++ app/src/main/java/com/example/madt0621/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.madt0621;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMainLabel)
+    }
+
+    public void onBtnChangeClick(View view) {
+        this.tvMain.setText("We did something! Yay!");
     }
 }
